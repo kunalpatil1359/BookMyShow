3834177494306cd957d1741b7995e569bd547a28
Kunal Patil <kunalpatil1359@gmail.com>
2024-01-15 14:49:30 +0530

    first commit

---
+++ src/data.js
@@ -0,0 +1,7 @@
+export let moviesList = ['Dunki', 'Barbie', '12th Fail', 'Jawan', 'Tiger 3', "OMG 2", "Chalo India"];
+export let slots = ['10:00 AM', '01:00 PM', '03:00 PM', "08:00 PM", "11:30 PM"];
+export let seats = ['A1', 'A2', 'A3', 'A4', 'D1', 'D2'];
+
+// exports.moviesList = moviesList;
+// exports.slots = slots;
+// exports.seats = seats;
